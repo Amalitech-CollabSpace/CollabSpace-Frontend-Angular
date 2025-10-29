fdf4d00340b3b0315cf56c3bc0aaa54a0cd7cb6b
44 <6>
2025-10-29 11:05:19 +0000

    feat: add project-details component

---
+++ src/app/project-management/components/project-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-details',
+  standalone: true,
+  template: `
+    <div>
+      <!-- Project details component will be implemented here -->
+    </div>
+  `,
+  styles: []
+})
+export class ProjectDetailsComponent {
+  // Component logic will be implemented here
+}
