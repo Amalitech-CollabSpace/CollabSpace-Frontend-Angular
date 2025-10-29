714e531e19da0ff27e8a25c974c7fb7fcae0bd7e
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-10-29 19:14:46 +0000

    feat:search bar implemented

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { bootstrapBell, bootstrapPersonFill, bootstrapSearch } from '@ng-icons/bootstrap-icons';
+import { provideIcons, NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.scss',
+  viewProviders: [
+    provideIcons({
+  bootstrapSearch,bootstrapBell,bootstrapPersonFill
+    }),
+  ],
 })
 export class SearchBar {
 
--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from "../../components/side-bar/side-bar";
+import { SearchBar } from '../../components/search-bar/search-bar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, SideBar],
+  imports: [RouterOutlet, SideBar, SearchBar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
