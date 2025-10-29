702794b32d5d2108bce187df60ae8fa72440f24c
44 <6>
2025-10-29 11:05:14 +0000

    feat: add project-list component

---
+++ src/app/project-management/components/project-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list',
+  standalone: true,
+  template: `
+    <div>
+      <!-- Project list component will be implemented here -->
+    </div>
+  `,
+  styles: []
+})
+export class ProjectListComponent {
+  // Component logic will be implemented here
+}
