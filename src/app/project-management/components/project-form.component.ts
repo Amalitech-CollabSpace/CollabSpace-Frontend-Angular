4375783465ef13c3caf0352bbd5de9ae97902583
44 <6>
2025-10-29 11:05:16 +0000

    feat: add project-form component

---
+++ src/app/project-management/components/project-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-form',
+  standalone: true,
+  template: `
+    <div>
+      <!-- Project form component will be implemented here -->
+    </div>
+  `,
+  styles: []
+})
+export class ProjectFormComponent {
+  // Component logic will be implemented here
+}
