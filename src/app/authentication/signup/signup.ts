ac1fc0dcc891b219f2555a13f6d17e8521eeaf00
selinaKudadze <kudadzeselina@gmail.com>
2025-10-29 07:28:25 +0000

    feat:create interface for signedup user

--- src/app/authentication/signup/signup.ts
+++ src/app/authentication/signup/signup.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import {FormGroup,ReactiveFormsModule,FormControl} from '@angular/forms'
+import SignedUpUserInterface from '../user-interface';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
-export class Signup {
+export class Signup implements OnInit{
+
+  public signedUpUser!:FormGroup
+
+  ngOnInit(){
+    
+    this.signedUpUser=new FormGroup({
+      firstName:new FormControl(''),
+      lastName:new FormControl(''),
+      email:new FormControl(''),
+      password:new FormControl(''),
+      confirmPassword:new FormControl(''),
+    })
+
+  }
+
 
 }
