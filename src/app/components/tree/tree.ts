7a5bc05811c94a8a1ccdcd7c45085bbcbd72b6cd
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-10-30 20:06:10 +0000

    feat:tree component added

---
+++ src/app/components/tree/tree.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
+interface TreeNode {
+  name: string;
+  children?: TreeNode[];
+  route?: string;
+}
+
+@Component({
+  selector: 'app-tree',
+  imports: [MatTreeModule, MatButtonModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './tree.html',
+  styleUrl: './tree.scss',
+})
+export class Tree {
+   @Input() dataSource: TreeNode[] = [];
+
+  childrenAccessor = (node: TreeNode) => node.children ?? [];
+
+  hasChild = (_: number, node: TreeNode) => !!node.children && node.children.length > 0;
+}
+
+const EXAMPLE_DATA: TreeNode[] = [
+  {
+    name: 'Fruit',
+    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+      },
+      {
+        name: 'Orange',
+        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+      },
+    ],
+  },
+];
