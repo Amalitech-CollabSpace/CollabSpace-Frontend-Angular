fa536329e85fbc513f9775704fcd044d455ed55e
selinaKudadze <kudadzeselina@gmail.com>
2025-10-30 07:41:50 +0000

    fix:allow users to hide and view passwords Change ui of button

--- src/app/authentication/input-component/input-component.ts
+++ src/app/authentication/input-component/input-component.ts
@@ -1,17 +1,22 @@
 import { Component,Input} from '@angular/core';
 import {FormControl,ReactiveFormsModule} from '@angular/forms'
-import {KeyValuePipe} from '@angular/common';
+import { KeyValuePipe, NgClass } from '@angular/common';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroUsers,heroEye,heroEyeSlash } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-input-component',
-  imports: [ReactiveFormsModule,KeyValuePipe],
+  imports: [ReactiveFormsModule, KeyValuePipe, NgClass,NgIcon],
   templateUrl: './input-component.html',
   styleUrl: './input-component.scss',
+  viewProviders: [provideIcons({  heroUsers,heroEye,heroEyeSlash })]
 })
 export class InputComponent {
   @Input() controlName = new FormControl('');
   @Input() label:string='';
-  @Input() type:string=''
+  @Input() type:string='';
+
+  showPassword:boolean=true
 
 
   public errorMessages:Record<string, string>={
@@ -19,6 +24,11 @@ export class InputComponent {
     pattern:'Please enter a valid email',
     
   
+  }
+
+  togglePasswordVisibility():void{
+    this.showPassword=!this.showPassword;
+
   }
   
   
