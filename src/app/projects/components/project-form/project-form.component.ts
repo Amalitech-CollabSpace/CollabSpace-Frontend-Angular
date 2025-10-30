fca8ffdcb1c27586297bf9866f0ccfa54bf1542c
44 <6>
2025-10-30 17:01:21 +0000

    refactor(projects): migrate ProjectFormComponent to reactive forms

--- src/app/projects/components/project-form/project-form.component.ts
+++ src/app/projects/components/project-form/project-form.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-project-form',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './project-form.component.html',
   styleUrl: './project-form.component.scss'
 })
-export class ProjectFormComponent {
-  // Component logic will be implemented here
+export class ProjectFormComponent implements OnInit {
+  projectForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.projectForm = this.fb.group({
+      name: ['', [Validators.required]],
+      description: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.projectForm.valid) {
+      const project: Project = this.projectForm.value;
+      console.log('Project data:', project);
+    }
+  }
 }
 
