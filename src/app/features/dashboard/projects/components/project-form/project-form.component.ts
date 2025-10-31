297c90448347e50454cda89987f347b2bd59216b
44 <6>
2025-10-31 12:09:41 +0000

    fix(projects): remove console logs from project form

--- src/app/features/dashboard/projects/components/project-form/project-form.component.ts
+++ src/app/features/dashboard/projects/components/project-form/project-form.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Project } from '../../../../../models/project.model';
+import { InputComponent } from '../../../../../components/input-component/input-component';
 
 @Component({
   selector: 'app-project-form',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, InputComponent],
   templateUrl: './project-form.component.html',
   styleUrl: './project-form.component.scss'
 })
@@ -26,7 +27,6 @@ export class ProjectFormComponent implements OnInit {
   onSubmit(): void {
     if (this.projectForm.valid) {
       const project: Project = this.projectForm.value;
-      console.log('Project data:', project);
     }
   }
 }
