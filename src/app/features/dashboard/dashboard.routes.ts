44cea3f37a5b00d2712507b507e4b19d0a6236f9
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-10-31 14:07:40 +0000

    refactor:tasks removed

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -25,11 +25,6 @@ export const routes: Routes = [
           import('./projects/projects').then((m) => m.Projects),
       },
       {
-        path: 'tasks',
-        loadComponent: () =>
-          import('./tasks/tasks').then((m) => m.Tasks),
-      },
-      {
         path: 'analytics',
         loadComponent: () =>
           import('./analytics/analytics').then((m) => m.Analytics),
