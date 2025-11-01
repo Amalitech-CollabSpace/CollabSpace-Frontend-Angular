3d28297d5a15c1fa889c32703920df1c15870418
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2025-11-01 19:40:50 +0000

    feat:draggable data table created for tasks list view

---
+++ src/app/components/dad-table/dad-table.ts
@@ -0,0 +1,103 @@
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  input,
+  signal,
+  effect,
+} from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  MatTable,
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  DragDropModule,
+} from '@angular/cdk/drag-drop';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapSearch } from '@ng-icons/bootstrap-icons';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { Task } from '../../models/task';
+
+
+@Component({
+  selector: 'app-dad-table',
+  standalone: true,
+  templateUrl: './dad-table.html',
+  styleUrls: ['./dad-table.scss'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    DragDropModule,
+    NgClass,
+    NgIcon,
+  ],
+  viewProviders: provideIcons({ bootstrapSearch }),
+})
+export class DadTable implements AfterViewInit {
+  @ViewChild(MatTable, { static: true }) table!: MatTable<Task>;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  data = input<Task[]>([]);
+  displayedColumns = input<string[]>([
+    'id',
+    'title',
+    'status',
+    'dueDate',
+    'assignee',
+    'actions',
+  ]);
+  showSearch = input<boolean>(true);
+
+ 
+  searchTerm = signal('');
+  dataSource = new MatTableDataSource<Task>([]);
+
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.data();
+      this.applyFilter();
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+
+  applyFilter() {
+    const filterValue = this.searchTerm().trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+
+  drop(event: CdkDragDrop<Task[]>) {
+    const previousData = [...this.dataSource.data];
+    moveItemInArray(previousData, event.previousIndex, event.currentIndex);
+    this.dataSource.data = previousData;
+  }
+
+
+  updateSearchTerm(value: string) {
+    this.searchTerm.set(value);
+    this.applyFilter();
+  }
+}
