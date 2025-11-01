f112f423e4fb0b9d2e97a1bbd2ad8dec939c2bf3
selinakudadze <kudadzeselina@gmail.com>
2025-11-01 13:37:19 +0000

    fix:use prettier to format code

--- src/app/features/auth/signup/signup.ts
+++ src/app/features/auth/signup/signup.ts
@@ -1,87 +1,95 @@
-import { Component,OnInit, inject,ViewChild,OnDestroy} from '@angular/core';
-import {RouterLink,Router} from "@angular/router";
-import {ReactiveFormsModule,FormControl,FormGroup,Validators} from "@angular/forms";
-import {confirmPasswordValidator} from '../validators/confirmPassword';
-import {InputComponent} from '../../../components/input-component/input-component';
-import {AuthServices} from '../../../core/services/authService/auth-service';
-import {Subject, takeUntil} from 'rxjs'
+import { Component, OnInit, inject, ViewChild, OnDestroy } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { confirmPasswordValidator } from '../validators/confirmPassword';
+import { InputComponent } from '../../../components/input-component/input-component';
+import { AuthServices } from '../../../core/services/authService/auth-service';
+import { Subject, takeUntil } from 'rxjs';
 
-import {StrongPasswordValidator} from '../validators/passwordRegex';
+import { StrongPasswordValidator } from '../validators/passwordRegex';
 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-import {toast,NgxSonnerToaster} from 'ngx-sonner';
+import { toast, NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-signup',
-  imports: [ReactiveFormsModule, InputComponent, RouterLink,NgxSonnerToaster,MatProgressSpinnerModule],
+  imports: [
+    ReactiveFormsModule,
+    InputComponent,
+    RouterLink,
+    NgxSonnerToaster,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './signup.html',
   // styleUrl: '',
 })
-export class Signup implements OnInit,OnDestroy {
-  private router=inject(Router);
-  private authService=inject(AuthServices)
-  private _destroy$=new Subject<void>();
-  public isLoading=false;
-  
+export class Signup implements OnInit, OnDestroy {
+  private router = inject(Router);
+  private authService = inject(AuthServices);
+  private _destroy$ = new Subject<void>();
+  public isLoading = false;
 
-  
+  signUpForm!: FormGroup<{
+    fullName: FormControl<string | null>;
+    email: FormControl<string | null>;
+    password: FormControl<string | null>;
+    confirmPassword: FormControl<string | null>;
+  }>;
 
-  signUpForm!:FormGroup<{fullName:FormControl<string|null>,email:FormControl<string|null>,password:FormControl<string|null>,confirmPassword:FormControl<string|null>}>;
-
-
-  ngOnInit(){
-    this.signUpForm=new FormGroup({
-      fullName:new FormControl('',[Validators.required]),
-      email:new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-      password:new FormControl('',[Validators.required,Validators.minLength(8),StrongPasswordValidator()
-]),
-      confirmPassword:new FormControl('',[Validators.required]),
-
-    },{validators:confirmPasswordValidator});
+  ngOnInit() {
+    this.signUpForm = new FormGroup(
+      {
+        fullName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          StrongPasswordValidator(),
+        ]),
+        confirmPassword: new FormControl('', [Validators.required]),
+      },
+      { validators: confirmPasswordValidator }
+    );
   }
 
-  
+  public submitSignup() {
+    this.isLoading = true;
 
-  public submitSignup(){
-    this.isLoading=true
-    
-    if(this.signUpForm.valid){
-      const newUser={
-        fullName:this.signUpForm.controls.fullName.value || '',
-        password:this.signUpForm.controls.password.value || '',
-        email:this.signUpForm.controls.email.value||'',
-      }
-      this.authService.signup(newUser)
-      .subscribe({
-        next:()=> {
-          this.isLoading=false
-          this.router.navigate(['/dashboard'])
-          toast.success("Registered successfully")
-          
-          takeUntil(this._destroy$)
+    if (this.signUpForm.valid) {
+      const newUser = {
+        fullName: this.signUpForm.controls.fullName.value || '',
+        password: this.signUpForm.controls.password.value || '',
+        email: this.signUpForm.controls.email.value || '',
+      };
+      this.authService.signup(newUser).subscribe({
+        next: () => {
+          this.isLoading = false;
+          this.router.navigate(['/dashboard']);
+          toast.success('Registered successfully');
+
+          takeUntil(this._destroy$);
         },
-        error:(err)=>{
-          this.isLoading=false
-          toast.error(err?.error?.error||err?.message||'Unknown error')
-          
+        error: (err) => {
+          this.isLoading = false;
+          toast.error(err?.error?.error || err?.message || 'Unknown error');
         },
-})
-      
-      
-  }else{
+      });
+    } else {
       this.signUpForm.markAllAsTouched();
+    }
   }
-}
 
-
-ngOnDestroy(){
+  ngOnDestroy() {
     this._destroy$.next();
     this._destroy$.complete();
   }
-
-  
-   
-
-
 }
