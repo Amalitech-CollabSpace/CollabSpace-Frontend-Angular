2ad1eb6d12c42f344fee1c5f2c5e79590848e45c
selinakudadze <kudadzeselina@gmail.com>
2025-11-01 21:58:37 +0000

    feat:create base service for socket.io integration

--- src/app/core/services/socketService/socket-service.ts
+++ src/app/core/services/socketService/socket-service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class SocketService {
-  
+export class SocketService implements OnInit {
+  private socket!: Socket;
+
+  ngOnInit(): void {
+    this.socket = io('');
+  }
+
+  sendComment(comment: string): void {
+    this.socket.emit('comment', comment);
+  }
+
+  onComment(callback: (comment: string) => void): void {
+    this.socket.on('comment', callback);
+  }
 }
