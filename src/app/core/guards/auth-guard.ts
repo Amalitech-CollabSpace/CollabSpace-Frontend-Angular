8982b97bf9542206c237cc4f48c0fba7e611f041
selinakudadze <kudadzeselina@gmail.com>
2025-11-01 13:30:59 +0000

    fix:make authGuard more secure by navigating the user to login page and after successful login, redirecting to the last visited page after login

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,13 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn,Router } from '@angular/router';
 import {AuthServices} from '../services/authService/auth-service'
 import {inject} from '@angular/core'
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const authService=inject(AuthServices)
+  const authService=inject(AuthServices);
+  const router=inject(Router);
+
 
   if(authService.isLoggedIn()){
     return true
   }
 
-  return false;
+  return router.createUrlTree(['/auth/login'],{queryParams:{returnUrl:state.url}})
 };
