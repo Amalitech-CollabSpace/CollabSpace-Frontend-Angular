c30e31eeb3c8ad1010a60b78913d3ff598486d23
44 <6>
2025-11-06 12:49:56 +0000

    feat(us-19): implement report export feature (CSV/PDF) with download support

--- src/app/features/dashboard/analytics/analytics.ts
+++ src/app/features/dashboard/analytics/analytics.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ExportReportButtonComponent } from '../components/export-report-button/export-report-button.component';
 
 @Component({
   selector: 'app-analytics',
-  imports: [],
+  imports: [ExportReportButtonComponent],
   templateUrl: './analytics.html',
   styleUrl: './analytics.scss',
 })
